a45c95aef311ad879ca4e53e2a6d76d5878d93d2
Yamilet <yamidg@yahoo.com>
2016-10-01 07:24:37 +0000

    change index

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 module.exports = {
     "entry": [
-        './index.js'
+        './app/index.js'
     ],
     "output": {
         "path": path.join(__dirname, 'build'),
